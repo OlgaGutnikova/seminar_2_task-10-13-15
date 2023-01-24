6423881ea4440518a62ede8eca913ae69019c640
Gutnikova Olga <olichikf1@mail.ru>
2023-01-24 11:30:08 +0300

    решение задачи 28

---
+++ task_28/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getProizvedenie(int end)
+{
+    int Proizvedenie = 1;
+    for (int i = 1; i <= end; i++)
+    {
+        Proizvedenie = Proizvedenie * i;
+    }
+    return Proizvedenie;
+}
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(data);
+    Console.ResetColor();
+}
+int end = getUserData("Введите число А для получения произведения");
+int Proizvedenie = getProizvedenie(end);
+Console.Write(Proizvedenie);
---
+++ task_30/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+int getUserNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int[] generateArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+
+int N = getUserNumber("Введите количество элементов массива");
+int[] Array = generateArray(N);
+PrintArray(Array);
