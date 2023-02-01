f2f771dde0b926b5d2ae673c27dc4ed58a5e19a7
Gutnikova Olga <olichikf1@mail.ru>
2023-02-01 11:24:14 +0300

    Решение задачи 56

---
+++ task_56/Program.cs
@@ -0,0 +1,81 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей 
+суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+int[,] generate2DArray(int rowLength, int colLength, int start, int finish)
+{
+    int[,] array = new int[rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            array[i, j] = new Random().Next(start, finish + 1);
+        }
+    }
+    return array;
+}
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void print2Darray(int[,] array)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printInColor(j + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    System.Console.WriteLine("-----------------------------------");
+}
+
+int[] sumLineElements(int[,] array)
+{
+    int[] sum = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum[i] += array[i, j];
+        }
+    }
+    return sum;
+}
+
+int minLine(int[] array)
+{
+    int minSumLine = array[0];
+    int minLineIndex = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < minSumLine)
+        {
+            array[i] = minSumLine;
+            minLineIndex = i;
+        }
+    }
+    return minLineIndex;
+}
+
+int[,] array = generate2DArray(4, 5, 0, 10);
+print2Darray(array);
+int[] sumElements = sumLineElements(array);
+int minLineS = minLine(sumElements);
+System.Console.WriteLine($"Строка с наименьшей суммой элементов: {minLineS}");
